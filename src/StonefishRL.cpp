8bcd0a1ed168a5239654b79b6533d16506f88ed8
Pau Font Pérez <163506517+PauFont3@users.noreply.github.com>
2025-06-12 19:03:48 +0200

    Add files via upload
    - Falta comprovar que els valors que dona la funció "getObservations()" son correctes.
    - No apareix l'error "segmentation fault" o "Violación de segmento (`core' generado)" al executar.
    - No fa cap step, per aixo no se si els valors que ens dona son els correctes o no.

--- src/StonefishRL.cpp
+++ src/StonefishRL.cpp
@@ -1,25 +1,37 @@
 #include "StonefishRL.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <cmath> // Per fer servir std::sin
 
-#include <Stonefish/core/SimulationManager.h>
-#include <Stonefish/core/ScenarioParser.h> // Per carregar l'arxiu XML
-#include <Stonefish/sensors/scalar/RotaryEncoder.h> // Per fer sf::RotaryEncoder
-#include <Stonefish/actuators/Motor.h> // Per fer sf::Motor
-
+// Includes per SENSORS
 #include <Stonefish/sensors/Sensor.h> // Per la clase base Sensor i el getName()
 #include <Stonefish/sensors/Sample.h> // Per poder fer sf:Sample
 #include <Stonefish/sensors/ScalarSensor.h> // Per la clase base ScalarSensor i el getLastSample()
+#include <Stonefish/sensors/VisionSensor.h>
+#include <Stonefish/sensors/scalar/IMU.h> // IMU es vectorial, pero esta en aquesta ruta
+#include <Stonefish/sensors/scalar/RotaryEncoder.h> // Per fer sf::RotaryEncoder
+#include <Stonefish/sensors/vision/Camera.h>
 
+// Includes per ACTUADORS
+#include <Stonefish/actuators/Servo.h>
+#include <Stonefish/actuators/Motor.h>
+#include <Stonefish/actuators/Actuator.h>
+
+#include <Stonefish/core/SimulationManager.h>
+#include <Stonefish/core/ScenarioParser.h> // Per carregar l'arxiu XML
+#include <Stonefish/StonefishCommon.h>
 
 
 
 // Constructor de la classe StonefishRL
-StonefishRL::StonefishRL(double frequency, const std::string& path) 
+StonefishRL::StonefishRL(const std::string& path, double frequency)
     : sf::SimulationManager(frequency), scenePath(path)
 { 
     std::cout << "Constructor de StonefishRL" << std::endl;
-    std::cout << "[INIT] StonefishRL created with scene: " << scenePath << std::endl;
+    std::cout << "[INIT] StonefishRL created with scene: " 
+              << path << std::endl;
 }   
 
 // Destructor
@@ -41,24 +53,150 @@ void StonefishRL::Reset() {
     std::cout << "[StonefishRL] Scenario restarted OK\n";
 }
 
-void StonefishRL::Step(double ts) {
+void StonefishRL::Step(sf::Scalar ts) {
     sf::SimulationManager::StepSimulation(ts);
     std::cout << "[StonefishRL] StepSimulation OK";
 }
 
 void StonefishRL::ApplyCommands(const CommandData& cmds) {
+ 
+    for(auto const& [name, val] : cmds.commands) {
+        std::cout << "NOM ACTUADOR: " << name << ". Valors: " << val << "." << std::endl;
+        
+        auto iT = actuators_.find("Robot/Servo");
+        
+        if(iT == actuators_.end()) {
+            std::cout << "[TESTS] Actuator name:" << name << " not registered" << std::endl;
+            //continue;
+        }
+
+        auto act = iT->second;
+        if(act->getType() == sf::ActuatorType::SERVO){
+            std::cout << "He entrat" << std::endl;
+            sf::Servo* servo = (sf::Servo*)(act);
+            servo->setControlMode(sf::ServoControlMode::POSITION);
+            servo->setDesiredPosition(9.5);
+        }
+    }   
+
+// DIU QUE NO TROBA ELS ACTUADORS
+/*    for(const auto& pair : cmds.commands) {
+        const std::string& actuator_name = pair.first;
+        float command_value = pair.second;
     
+        // Buscar actuador per nom
+        auto iT = actuators_.find(actuator_name);
+        if(iT == actuators_.end()) {
+            std::cerr << "[WARN] Actuator '" << actuator_name << "' not found in simulation." << std::endl;
+            continue;
+        }
+
+        sf::Actuator* generic_actuator = iT->second;
+
+        if(auto* servo = dynamic_cast<sf::Servo*>(generic_actuator)) {
+            servo->setControlMode(sf::ServoControlMode::POSITION);
+            servo->setMaxVelocity(0.1f);
+            servo->setDesiredPosition(command_value);
+            // ...
+        }
+
+     // Per afegir més tipus 
+     // else if(auto* motor = dynamic_cast<sf::Motor*>(actuator)) {
+     //     motor->setIntensity(value);
+     //
+     // }
+
+        else {
+            std::cerr << "[WARN] Actuator '" << actuator_name << "' type not supported in ApplyCommands." << std::endl;
+        }
+    }
+
+*/
+/*   
+    float t = getSimulationTime();
+    float pos = 0.5 * std::sin(t);
+
+
+    for(const auto& pair : cmds.commands) 
+    {
+        const std::string& actuator_name = pair.first;
+        const float command_value = pair.second;
+    
+    
+        auto iT = actuators_.find(actuator_name);
+        if(iT == actuators_.end())
+        {
+            std::cerr << "[WARN] Actuator " << actuator_name << " not found in simulation." << std::endl;
+        }
+
+        sf::Actuator* generic_actuator = iT->second;
+
+        if(auto motor = dynamic_cast<sf::Motor*>(generic_actuator)){
+            
+        }
+        else if(auto servo = dynamic_cast<sf::Servo*>(generic_actuator)){
+            
+        }
+    }
+*/
 }
 
-StonefishRL::ObsData StonefishRL::GetObservations() {
-    ObsData data; // Per guardar les dades
 
+StonefishRL::ObsData StonefishRL::GetObservations() {
+    
+    ObsData data;
+
+    if (obs_sensors_.empty()) {
+        return data;
+    }
+
+    for(const auto& [sensor_name, sensorPtr] : obs_sensors_) {
+
+        if(!sensorPtr) {
+            std::cerr << "[WARN] Sensor " << sensor_name << " is nullptr!" << std::endl;
+            continue;
+        }
+        
+        // ScalarSensor
+        if(auto scalar_sensor = dynamic_cast<sf::ScalarSensor*>(sensorPtr))
+        {
+            //std::vector<float> sensor_values = scalar_sensor->getLastSample().getData();
+            sensor_values_[sensor_name] = scalar_sensor->getLastSample().getData();
+            std::cout << "Tamany del vector:" << sensor_values_[sensor_name].size() << std::endl;
+
+            for(unsigned int i = 0; i < sensor_values_[sensor_name].size(); i++) {
+                std::cout << "Valors del sensors: " << sensor_values_[sensor_name][i] << std::endl;
+            }
+
+        // ------------------ PROBABLEMENT OPCIONAL ------------------
+            sf::Sample last_sample = scalar_sensor->getLastSample();
+            std::cout << "[INFO] LastSample ID: " << last_sample.getId() << std::endl 
+                      << "[INFO] Sensor Name: " << sensor_name << std::endl 
+                      << "[INFO] Num of channels: " << scalar_sensor->getNumOfChannels() << std::endl;
+       
+            for (size_t i = 0; i < last_sample.getNumOfDimensions(); i++) {
+                std::cout << " Dim " << i << ": " << last_sample.getValue(i) << std::endl;
+            }
+         // ------------------------------------------------------------
+       
+            if (scalar_sensor->getNumOfChannels() == 0) {
+                std::cerr << "[WARN] Sensor " << sensor_name << " has zero channels." << std::endl;
+                continue;
+            }
+
+            if (sensor_values_.empty()) {
+                std::cerr << "[WARN] No observations catched! (Check if sensors are defined in XML)" << std::endl;
+            }
+        }
+    }
+
+/*
     // Iterar sobre els sensors que s'han guardat durant BuildScenario(). 
     for(auto const& [name, sensor] : obs_sensors_) 
     {
         // Falta controlar errors del nullptr per exemple
-        if(auto* scalarSensor = dynamic_cast<sf::ScalarSensor*>(sensor)){
-            
+        if(auto* scalarSensor = dynamic_cast<sf::ScalarSensor*>(sensor))
+        {
             // Agafar la ultima mostra del ScalarSensor
             sf::Sample lastSample = scalarSensor->getLastSample();
 
@@ -75,66 +213,79 @@ StonefishRL::ObsData StonefishRL::GetObservations() {
             // Guardar el vector al map d'observacions
             data.num_observations[name] = sensor_values;
         }
-        else {
+        else 
+        {
             std::cout << "Sensor: " << sensor->getName() << " not detected" << std::endl;
         }
 
-        // else if (sensor Imatge)
+        // else if (vision sensor)
 
     }
-
+*/
     return data;
 }
 
 void StonefishRL::BuildScenario() {
 
     std::cout << "[INFO] Building scenario from: " << scenePath << std::endl;
-
     sf::ScenarioParser parser(this);
-
-    if (parser.Parse(scenePath)) 
-    {
-        std::cout << "Ha entrat al buildscenario" << std::endl;
-        /*
-        sf::Robot* robot;
-        unsigned int id = 0;
-        while((robot = getRobot(id++)) != nullptr){
-            std::cout << "Num robots: " << id << std::endl;
-        }   
-        */
-
-        sf::Sensor* sensor_ptr;
-        unsigned int sensor_id = 0;
-        while((sensor_ptr = getSensor(sensor_id++)) != nullptr){
-            obs_sensors_[sensor_ptr->getName()] = sensor_ptr;
-        }  
-        std::cout << "Contingut del mapa de sensors:" << std::endl;
-        for (const auto& pair : obs_sensors_) {
-            std::cout << "Clau: '" << pair.first << "', punter: " << pair.second << std::endl;
-        }
-
-        
-        sf::Actuator* actuator_ptr;
-        unsigned int actuator_id = 0;
-        while((actuator_ptr = getActuator(actuator_id++)) != nullptr){
-            actuators_[actuator_ptr->getName()] = actuator_ptr;
-        }  
-        std::cout << "Contingut del mapa d'actuadors:" << std::endl;
-        for (const auto& pair : actuators_) {
-            std::cout << "Clau: '" << pair.first << "', punter: " << pair.second << std::endl;
-        }
-
-
-        
-        std::cout << "[INFO] Scenario loaded succesfully.\n";
-    } 
     
-    else 
-    {
+    if(!parser.Parse(scenePath)){
         std::cerr << "[ERROR] Error charging the scenario: " << scenePath << "\n";
         for (const auto& msg : parser.getLog()) {
             std::cerr << "[ScenarioParser Log] " << msg.text << "\n";
         }
+        return;
     }
 
+    std::cout << "Ha entrat al BuildScenario()" << std::endl;
+/*
+    sf::Robot* robot_ptr;
+    unsigned int id = 0;
+    while((robot_ptr = getRobot(id++)) != nullptr){
+        std::cout << "Nom robot: " << robot_ptr->getName() << std::endl;
+    }   
+*/  
+
+    obs_sensors_.clear();
+    actuators_.clear();
+
+    sf::Sensor* sensor_ptr;
+    unsigned int sensor_id = 0;
+
+    while((sensor_ptr = getSensor(sensor_id++)) != nullptr) {
+        obs_sensors_[sensor_ptr->getName()] = sensor_ptr;
+    }  
+
+    
+    if(obs_sensors_.empty()) std::cout << "[WARN] No sensors registered in this scenario!" << std::endl;
+    else {
+        std::cout << "Contingut del mapa de sensors:" << std::endl;
+        for (const auto& pair : obs_sensors_) {
+            std::cout << "  Clau: '" << pair.first 
+                      << "\n        Valor UpdatedFrequency: " << pair.second->getUpdateFrequency() 
+                      << "\n        Valor Nom Sensor: " << pair.second->getName()
+                      << std::endl;
+        }
+    }
+    
+    sf::Actuator* actuator_ptr;
+    unsigned int actuator_id = 0;
+    while((actuator_ptr = getActuator(actuator_id++)) != nullptr){
+       
+        actuators_[actuator_ptr->getName()] = actuator_ptr;
+        
+    } 
+
+    if (actuators_.empty()) std::cout << "[WARN] No actuators registered in this scenario!" << std::endl;
+    else {
+        std::cout << "  Contingut del mapa d'actuadors:" << std::endl;
+        for (const auto& pair : actuators_) {
+            std::cout << "      Clau: '" << pair.first << "', punter: " << pair.second << std::endl;
+        }
+    }
+
+    ObsData ab = GetObservations();
+    
+    std::cout << "[INFO] Scenario loaded succesfully.\n";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,171 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <cmath> // Per fer std::sin(..)
+
 #include <Stonefish/core/GraphicalSimulationApp.h>
 #include <Stonefish/core/SimulationApp.h>
 #include <Stonefish/core/SimulationManager.h>
 #include <Stonefish/sensors/scalar/RotaryEncoder.h>
-#include <Stonefish/actuators/Motor.h>
+#include <Stonefish/sensors/scalar/IMU.h>
+#include <Stonefish/sensors/ScalarSensor.h>
 #include <Stonefish/sensors/Sample.h>
+
+#include <Stonefish/actuators/Motor.h>
+#include <Stonefish/actuators/Servo.h>
+
+#include <Stonefish/StonefishCommon.h>
+
+
+
+struct LearningThreadData
+{
+    sf::SimulationApp& sim;
+};
+
+
+int learning(void* data) {
+    sf::SimulationApp& simApp = static_cast<LearningThreadData*>(data)->sim;
+    sf::SimulationManager* simManager = simApp.getSimulationManager();
+
+    while (simApp.getState() == sf::SimulationState::NOT_READY)
+    {
+        SDL_Delay(10);
+    }
+    
+    simApp.StartSimulation();
+
+    while(simApp.getState() != sf::SimulationState::FINISHED) 
+    {
+
+        simApp.StepSimulation();
+
+        sf::Scalar angle1 = static_cast<sf::RotaryEncoder*>(simManager->getSensor("Robot/Encoder"))->getLastSample().getValue(0);
+    
+        sf::Scalar command1 = btCos(angle1) * 15;
+        sf::Scalar command2 = btCos(angle1) * 30;
+
+        //static_cast<sf::Actuator*>(simManager->getActuator("Servo"))->setIntensity(command1);
+
+
+    }
+
+    return 0;
+}
 
 
 int main(int argc, char **argv) {
 
-
     double frequency = 1000.0;
-    std::string dataDirPath = "scenes/minimal_learning.xml";//"../scenes/minimal_learning.xml";
-    if (argc > 1) {
-        dataDirPath = argv[1]; // Permet carregar l'escena des d'un argument en la linea de comandes 
+    
+    if (argc < 2) {
+        std::cerr << "[ERROR] You may need 1 arguments at least." << std::endl;
+        std::cerr << "Utilitzat: " << argv[0] << " <Resources/type_scene/scene.xml>\n";
+        return 1;
     }
-    
-    sf::RenderSettings r;
+
+    std::string scene_path = argv[1]; 
+
     sf::HelperSettings h;
+    sf::RenderSettings r;
+    r.windowW = 1200;
+    r.windowH = 900;
+   
 
-    StonefishRL* simulationManager = new StonefishRL(frequency, dataDirPath);
-    sf::GraphicalSimulationApp app("DEMO STONEFISH RL", dataDirPath, r, h, simulationManager);
+
+    StonefishRL* simManager = new StonefishRL(scene_path, frequency);
+
+    sf::GraphicalSimulationApp app("DEMO STONEFISH RL", scene_path, r, h, simManager);
+
+    LearningThreadData data {app};
+    SDL_Thread* learningThread = SDL_CreateThread(learning, "learningThread", &data);
+/*
+    app.StartSimulation();
+
+    unsigned int comptador = 0;
+
+    while(app.getState() != sf::SimulationState::FINISHED && comptador< 10)
+    {
+
+        app.StepSimulation(); // Fa donar un segmentation fault
+
+        float t = simManager->getSimulationTime();
+        std::cout << "[TEMPS SIMULACIO] " << t << std::endl;
+        StonefishRL::CommandData cmd;
+        
+        cmd.commands["Servo"] = 0.5 * std::cos(t);
+        cmd.commands["Servo2"] = -0.5 * std::cos(t);
     
-    //std::cout << sf::GetDataPath() << std::endl;
+     
+        simManager->ApplyCommands(cmd);
+        
+        StonefishRL::ObsData obs = simManager->GetObservations();
+
+        if(auto* enc = static_cast<sf::RotaryEncoder*>(simManager->getSensor("Encoder"))){
+            double angle = enc->getLastSample().getValue(0);
+            std::cout << "Encoder angle: " << angle << std::endl;
+        }
+        comptador++;
+   
+        simManager->Step(0.001); // Avançar 1ms
+        std::cout << "Fins aqui arriba" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Esperar 100ms per veure els resultats
+    }
+
+
+
+    /*
+    while(app.getState() != sf::SimulationState::FINISHED){
+        
+
+        app.StepSimulation();
+
+        // Get Observations
+        sf::Scalar angle1 = static_cast<sf::RotaryEncoder*>(simManager->getSensor("Encoder1"))->getLastSample().getValue(0);
+
+
+        static_cast<sf::Motor*>(simManager->getActuator("Servo"))->setIntensity(0.75);
+    }
+
+        if(auto *enc = static_cast<sf::RotaryEncoder*>(simManager->getSensor("Encoder"))){
+            double angle = enc->getLastSample().getValue(0);
+            std::cout << "Encoder angle: " << angle << std::endl;
+        }      
+        
+      
+        if(auto *imu = static_cast<sf::IMU*>(simManager->getSensor("IMU"))){
+            auto sample = imu->getLastSample();
+            std::cout << "IMU ang vel: "
+                      << sample.angular_velocity.transpose()
+                      << "; lin acc: "
+                      << sample.linear_acceleration.transpose()
+                      << std::endl;
+        }
     
 
-    app.Run(false, false, /*1.0/frequency/*/sf::Scalar(0.1));
+        double t = simManager->getSimulationTime();
 
-    return 0;
+        auto* s1 = static_cast<sf::Servo*>(simManager->getActuator("Servo"));
+        if (s1) {
+            s1->setControlMode(sf::ServoControlMode::POSITION);
+            s1->setMaxVelocity(0.1f);             // Max velocitat (rad/s)
+            s1->setDesiredPosition(0.5 * sin(t)); // Posicio objetiu
+        }
 
-    // Clean-up
-    //int status {0};
-    //SDL_WaitThread(learningThread, &status);
-    //return status;
+        if(auto* servo2 = static_cast<sf::Servo*>(simManager->getActuator("Servo2"))){
+            servo2->setControlMode(sf::ServoControlMode::POSITION);
+            servo2->setMaxVelocity(0.1f);             // Max velocitat (rad/s)
+            servo2->setDesiredPosition(0.5 * sin(t)); // Posicio objetiu
+        }
 
+    }
+*/
+
+
+    
+    app.Run(false, false, sf::Scalar(0.1));
+
+    int status {0};
+    SDL_WaitThread(learningThread, &status);
+    return status;
 }
