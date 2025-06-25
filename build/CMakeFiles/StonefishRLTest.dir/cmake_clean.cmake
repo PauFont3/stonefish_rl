50a2df2ea6d4a6f60b1bfcb8cc744560fd833fb6
Pau Font Pérez <163506517+PauFont3@users.noreply.github.com>
2025-06-25 10:37:52 +0200

    Add files via upload
    Es pot fer un applyCommands, però el Reset i el Exit encara no funcionen bé.

---
+++ build/CMakeFiles/StonefishRLTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StonefishRLTest.dir/src/StonefishRL.cpp.o"
+  "CMakeFiles/StonefishRLTest.dir/src/StonefishRL.cpp.o.d"
+  "CMakeFiles/StonefishRLTest.dir/src/main.cpp.o"
+  "CMakeFiles/StonefishRLTest.dir/src/main.cpp.o.d"
+  "StonefishRLTest"
+  "StonefishRLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StonefishRLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
